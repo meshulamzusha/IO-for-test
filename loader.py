0297c7bc75ed7d30a15c56db33a6061c70be6b27
Your Name <you@example.com>
2025-11-25 11:29:18 +0200

    add: Function to get lines of a certain length from a file

--- loader.py
+++ loader.py
@@ -25,3 +25,21 @@ def count_lines(path: str) -> int:
             count = count + 1
 
     return count
+
+
+def load_long_lines(path: str, min_length: int) -> list[str]:
+    """
+    :param path: path to file
+    :param min_length: The length of all rows in the returned list will be greater than this parameter.
+    :return: A list of lines whose length is greater than min_length
+    """
+    lines = []
+
+    with open(path, "r", encoding="utf-8") as file:
+
+        for line in file:
+            cleaned = line.rstrip("\n")
+            if len(cleaned) > min_length:
+                lines.append(cleaned)
+
+    return lines
