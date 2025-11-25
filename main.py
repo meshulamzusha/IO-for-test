62410c788c34abed4f8ae38dacf793fea99f4bf9
Your Name <you@example.com>
2025-11-25 13:13:10 +0200

    function print menu

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+
+
+def print_menu() -> None:
+    print(
+        "1. Load file and display how many lines are loaded."
+        "2. Show the number of lines in file."
+        "3. Show the number of words in file."
+        "4. Show the number of vowels in file."
+        "5. Print lines whose length is greater than given parameter."
+        "6. search a word in a file"
+    )
+
+
